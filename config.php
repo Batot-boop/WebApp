c1e2344f40332dc4172a84ddd9518885412c7597
Batot_BooB <137377427+Batot-boop@users.noreply.github.com>
2026-05-04 17:25:50 +0300

    Change: API Key

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('RAPIDAPI_KEY', '105e88c40dmsha4458d8b30434eap1222b6jsn580b40576c91');
+define('RAPIDAPI_KEY', '061fac96c5msh52b8850e0076e29p1a1dbejsn36701e6a2fdc');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'movie_tracker');
 define('DB_USER', 'root');
